38d7091f5384a170fd0e10c10dd16deea62dcf78
johnmckenna-snd <ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDpa6B/irwN7xAiRc6pqo6GlShOsOGmFRBoQw1qoAlMv sndwrksdesign@gmail.com>
2021-02-11 16:04:21 -0800

    ok it may or may not turn the relay on

--- server.js
+++ server.js
@@ -1,20 +1,8 @@
-import { Gpio } from 'onoff';
-const led = new Gpio(17, 'out');       // Export GPIO17 as an output
-let stopBlinking = false;
+import { runPump, stopPump, unexportPump } from './src/run-pump';
 
-// Toggle the state of the LED connected to GPIO17 every 200ms
-const blinkLed = _ => {
-  if (stopBlinking) {
-    return led.unexport();
-  }
+runPump();
+setTimeout(() => stopPump(), 5000);
 
-  led.read()
-    .then(value => led.write(value ^ 1))
-    .then(_ => setTimeout(blinkLed, 200))
-    .catch(err => console.log(err));
-};
-
-blinkLed();
-
-// Stop blinking the LED after 5 seconds
-setTimeout(_ => stopBlinking = true, 5000);
+process.on('SIGINT', () => {
+	unexportPump();
+});
---
+++ src/run-pump.js
@@ -0,0 +1,16 @@
+import { Gpio } from 'onoff';
+const pump1 = new Gpio(26, 'out');
+
+export const runPump = () => {
+	pump1.read()
+		.then(value => pump1.write(1));
+};
+
+export const stopPump = () => {
+	pump1.read()
+		.then(value => pump1.write(0));
+};
+
+export const unexportPump = () => {
+	return pump1.unexport();
+};
