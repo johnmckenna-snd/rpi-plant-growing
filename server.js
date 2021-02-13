9f836472c77934f9407d705fc6102b23f5a8261c
johnmckenna-snd <ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDpa6B/irwN7xAiRc6pqo6GlShOsOGmFRBoQw1qoAlMv sndwrksdesign@gmail.com>
2021-02-13 15:30:50 -0800

    async in process

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 import { runPump1, stopPump1, unexportPump1 } from './src/run-pump';
+import regeneratorRuntime from 'regenerator-runtime';
 
 runPump1();
 setTimeout(() => stopPump1(), 2000);
 
-process.on('SIGINT', () => {
-	stopPump1();
-	unexportPump1();
+process.on('SIGINT', async () => {
+	await stopPump1();
+	await unexportPump1();
 	process.exit();
 });
