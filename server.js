696c3cbc31a3ae4608676286d8aa5f0f42e39ec9
johnmckenna-snd <ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDpa6B/irwN7xAiRc6pqo6GlShOsOGmFRBoQw1qoAlMv sndwrksdesign@gmail.com>
2021-02-12 20:51:59 -0800

    ok fixed server

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ import { runPump, stopPump, unexportPump } from './src/run-pump';
 
 runPump();
 setTimeout(() => stopPump(), 20000);
-unexportPump();
 
 process.on('SIGINT', () => {
 	unexportPump();
