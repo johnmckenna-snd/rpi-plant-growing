deb0da8dac09571d1518d27d25f8c76845a67711
johnmckenna-snd <ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDpa6B/irwN7xAiRc6pqo6GlShOsOGmFRBoQw1qoAlMv sndwrksdesign@gmail.com>
2021-02-13 14:11:25 -0800

    it helps if you save the file.

--- src/run-pump.js
+++ src/run-pump.js
@@ -4,12 +4,12 @@ const pump1 = new Gpio(26, 'out');
 export const runPump = () => {
 	console.log('runPump', pump1.read());
 	pump1.read()
-		.then(value => pump1.write(1));
+		.then(value => pump1.write(0));
 };
 
 export const stopPump = () => {
 	pump1.read()
-		.then(value => pump1.write(0));
+		.then(value => pump1.write(1));
 };
 
 export const unexportPump = () => {
