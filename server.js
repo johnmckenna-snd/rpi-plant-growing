14105c1f5a44e03160141476fa8a844cc3233214
johnmckenna-snd <ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDpa6B/irwN7xAiRc6pqo6GlShOsOGmFRBoQw1qoAlMv sndwrksdesign@gmail.com>
2021-02-13 15:00:00 -0800

    try catch

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import { runPump1, stopPump1, unexportPump1 } from './src/run-pump';
 
 runPump1();
 setTimeout(() => stopPump1(), 20000);
+unexportPump1();
 
 process.on('SIGINT', () => {
 	stopPump1();
--- src/run-pump.js
+++ src/run-pump.js
@@ -1,9 +1,14 @@
 import { Gpio } from 'onoff';
 const pump1 = new Gpio(26, 'out');
 
-export const runPump1 = () => {
-	pump1.read()
-		.then(value => pump1.write(0));
+export const runPump1 = async () => {
+	try {
+		await pump1.read();
+		await pump1.write(0);
+		console.log('Pump 1 Running');
+	} catch (e) {
+		throw e;
+	}
 };
 
 export const stopPump1 = () => {
