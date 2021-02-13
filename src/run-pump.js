6a2b0f79a8f8bf9e60ecd8e8378fcbeefec076c0
johnmckenna-snd <ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDpa6B/irwN7xAiRc6pqo6GlShOsOGmFRBoQw1qoAlMv sndwrksdesign@gmail.com>
2021-02-13 14:10:14 -0800

    attempting to change relay polarity. it's bad when the pi turns off.

--- src/run-pump.js
+++ src/run-pump.js
@@ -2,6 +2,7 @@ import { Gpio } from 'onoff';
 const pump1 = new Gpio(26, 'out');
 
 export const runPump = () => {
+	console.log('runPump', pump1.read());
 	pump1.read()
 		.then(value => pump1.write(1));
 };
