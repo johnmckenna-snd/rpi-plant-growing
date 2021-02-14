9542b0e569091ed6cc8ac82f6ed199c54030d39a
johnmckenna-snd <ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDpa6B/irwN7xAiRc6pqo6GlShOsOGmFRBoQw1qoAlMv sndwrksdesign@gmail.com>
2021-02-13 16:46:58 -0800

    added moisture sensor boilerplate

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 import { runPump1, stopPump1, unexportPump1 } from './src/run-pump';
+import { getMoistureSensor1 } from './src/get-moisture-level';
 import regeneratorRuntime from 'regenerator-runtime';
 
 runPump1();
 setTimeout(() => stopPump1(), 2000);
 
-process.on('SIGINT', async () => {
+getMoistureSensor1();
+
+process.on('SIGINT', async () => { // for ctrl + c
 	await stopPump1();
 	await unexportPump1();
 	process.exit();
---
+++ src/get-moisture-level.js
@@ -0,0 +1,13 @@
+import { Gpio } from 'onoff';
+import regeneratorRuntime from 'regenerator-runtime';
+
+const moistureSensor1 = new Gpio(2, 'in')
+
+export const getMoistureSensor1 = async () => {
+	try {
+		const moistureValue = await moistureSensor1.read();
+		console.log('Moisture Value is:', moistureValue);
+	} catch (e) {
+		throw e;
+	}
+};
