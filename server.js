b87290da019224bbd359bac2250151aa91a24fa9
johnmckenna-snd <ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDpa6B/irwN7xAiRc6pqo6GlShOsOGmFRBoQw1qoAlMv sndwrksdesign@gmail.com>
2021-02-13 17:26:00 -0800

    removed moisture sensor from server.

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
 import { runPump1, stopPump1, unexportPump1 } from './src/run-pump';
-import { getMoistureSensor1 } from './src/get-moisture-level';
 import regeneratorRuntime from 'regenerator-runtime';
 
 runPump1();
 setTimeout(() => stopPump1(), 2000);
-
-getMoistureSensor1();
 
 process.on('SIGINT', async () => { // for ctrl + c
 	await stopPump1();
